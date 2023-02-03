c746afa6f19b3b316f0cff8ff1361f1d445a4c78
Linh Tran <linhtran@Linhs-MacBook-Air.local>
2023-02-02 17:23:07 -0800

    update JS

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# work-day-scheduler
+# Work Day Scheduler
+
+## Description
+
+This week's challenge allows us to utilize our Third-Party API skills to work with a starter code that allows a user to save events for each hour of the working day. This starter code uses the Day.js library to work with date and time.  
+
+See below for the direct link to the live page:
+
+[Live Link to Deployed Application](INSERT)
+
+The following image demonstrates what the web application looks like once the user navigates to the live page. 
+
+![A preview of homepage.](INSERT)
--- assets/js/script.js
+++ assets/js/script.js
@@ -28,5 +28,28 @@ $(document).ready(function () {
   $("#hour-16 .description").val(localStorage.getItem("hour-16"));
   $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
+  // Checks the time and assigns past, present, future classes for color changes
+  function timeGrid() {
+    var timeNow = dayjs().hour();
 
+    $(".time-block").each(function () {
+      var checkTime = parseInt($(this).attr("id").split("hour-")[1]);
+
+      if (checkTime < timeNow) {
+        $(this).removeClass("present");
+        $(this).removeClass("future");
+        $(this).addClass("past");
+      } else if (checkTime === timeNow) {
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
+      }
+    })
+  }
+
+  timeGrid();
 });
